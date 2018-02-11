5a2a557f01deeaf6692bcf0fe727024ca25f787f
Toshimitsu Takahashi <toshi@tilfin.com>
2018-02-12 00:00:22 +0900

    Refactor main class

--- lib/konfig_yaml/main.rb
+++ lib/konfig_yaml/main.rb
@@ -1,13 +1,10 @@
 require 'pathname'
 
 # KonfigYaml main class
-# @attr [String] name Use this if log message is not specified (by default this is 'No message').
-# @attr [String] opts The field name of Exception (by default this is :err).
-# @attr [Hash] with_fields The fields appending to all logs.
-# @attr [Proc] before_log Hook before logging.
 class KonfigYaml
   include HashWrapper
-
+  # Create an configuration from yaml file
+  #
   # @param [String] name ('app') the basename of yaml file
   # @param [Hash] opts the options to intialize
   # @option opts [String] :env (ENV['RUBY_ENV'] || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development') run environment
@@ -21,41 +18,47 @@ class KonfigYaml
       opts = {}
     end
 
-    env = opts[:env] || ENV['RUBY_ENV'] || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
-    env = env.to_s
+    path = File.expand_path(opts[:path] || 'config', Dir.pwd)
+    env = environment(opts)
     use_cache = opts.fetch(:use_cache, true)
 
-    cfg_key = "#{name}/#{env}"
-    if use_cache && cfg_cache.key?(cfg_key)
-      @h = cfg_cache[cfg_key];
-      activate
-      return
-    end
-
-    dir_path = File.expand_path(opts[:path] || 'config', Dir.pwd)
-    cfg = load_file(name, dir_path);
-    if cfg.include?(env)
-      @h = deep_merge(cfg[env], cfg['default']);
-    else
-      @h = cfg['default']
-    end
-
-    raise ArgumentError.new("The configuration for #{env} is not defined in #{name}") unless @h
-
-    cfg_cache[cfg_key] = @h
-
+    load_config(name, env, path, use_cache)
     activate
+  end
+
+  # Clear caches
+  def self.clear
+    @@cfg_cache = {};
+  end
+
+  private
+
+  def environment(opts)
+    env = opts[:env]
+    return env.to_s if env
+    ENV['RUBY_ENV'] || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
   end
 
   def cfg_cache
     @@cfg_cache ||= {}
   end
 
-  def self.clear
-    @@cfg_cache = {};
-  end
+  def load_config(name, env, path, use_cache)
+    cfg_key = "#{name}/#{env}"
+    if use_cache && cfg_cache.key?(cfg_key)
+      @h = cfg_cache[cfg_key]
+      return
+    end
 
-  private
+    cfg = load_file(name, path);
+    if cfg.include?(env)
+      @h = deep_merge(cfg[env], cfg['default']);
+    else
+      @h = cfg['default']
+    end
+    raise ArgumentError.new("The configuration for #{env} is not defined in #{name}") unless @h
+    cfg_cache[cfg_key] = @h
+  end
 
   def load_file(name, dir)
     cfg_path = Dir.glob("#{dir}/#{name}.{yml,yaml}").first
@@ -64,8 +67,8 @@ class KonfigYaml
   end
 
   def deep_merge(target, default)
-    target = {} unless target
-    default = {} unless default
+    target ||= {}
+    default ||= {}
     target.merge!(default) do |key, target_val, default_val|
       if target_val.is_a?(Hash) && default_val.is_a?(Hash)
         deep_merge(target_val, default_val)
